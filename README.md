6b70268d38921e3edf91825a421627d4fe24cb29
tmaniak85 <tomasz.maniak.85>
2020-11-15 17:56:04 +0100

    Dodatkowy komentarz w kodzie oraz zdjęcie w readme.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Program wyświetlający imię
+
+![Image of Darth](https://www.partybox.pl/zasoby/images/middle/dekoracja_kartonowa_darth_vader_gwiezdne_wojny_29639.jpg)
--- src/Name.java
+++ src/Name.java
@@ -1,6 +1,6 @@
 public class Name {
 
     public static void main(String[] args) {
-        System.out.println("Tomasz");
+        System.out.println("Tomasz");//wyświetlenie napisu
     }
 }
